29ab9b738680b6bf928ef07cfa4401b3093e0135
rpdomingo <reynbowdomingo@gmail.com>
2019-06-25 08:54:29 +0800

    Add project files.

---
+++ Forms4/Forms4/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Forms4.Services;
+using Forms4.Views;
+
+namespace Forms4
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
